ff09bb3f396b25e1cad694057a6b631e6359ca64
Sofia Valadares Bastos <louisafolkens@gmail.com>
2025-10-24 17:59:51 -0300

    teste

--- monitoring.sh
+++ monitoring.sh
@@ -1,58 +1,57 @@
 #!/bin/bash
 
-# ARCHITECTURE
+# ARCH
 arch=$(uname -a)
 
 # CPU PHYSICAL
-cpuf=$(grep "physical id" /proc/cpuinfo | sort -u | wc -l)
+cpuf=$(grep "physical id" /proc/cpuinfo | wc -l)
 
 # CPU VIRTUAL
-cpuv=$(grep -c "processor" /proc/cpuinfo)
+cpuv=$(grep "processor" /proc/cpuinfo | wc -l)
 
 # RAM
-ram_total=$(free --mega | awk 'NR==2 {print $2}')
-ram_use=$(free --mega | awk 'NR==2 {print $3}')
-ram_percent=$(free --mega | awk 'NR==2 {printf "%.2f", $3/$2*100}')
+ram_total=$(LANG=C free --mega | awk '$1 == "Mem:" {print $2}')
+ram_use=$(LANG=C free --mega | awk '$1 == "Mem:" {print $3}')
+ram_percent=$(LANG=C free --mega | awk '$1 == "Mem:" {printf "%.2f", $3/$2*100}')
 
 # DISK
-disk_total=$(df -m | awk '/^\/dev\// && !/\/boot/ {disk_t += $2} END {printf "%.1fGb", disk_t/1024}')
-disk_use=$(df -m | awk '/^\/dev\// && !/\/boot/ {disk_u += $3} END {printf "%d", disk_u}')
-disk_percent=$(df -m | awk '/^\/dev\// && !/\/boot/ {disk_u += $3; disk_t += $2} END {printf "%d", disk_u/disk_t*100}')
+disk_total=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_t += $2} END {printf ("%.1fGb\n"), disk_t/1024}')
+disk_use=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_u += $3} END {print disk_u}')
+disk_percent=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_u += $3} {disk_t+= $2} END {printf("%d"), disk_u/disk_t*100}')
 
 # CPU LOAD
-cpu_idle=$(vmstat 1 2 | tail -1 | awk '{print $15}')
+cpul=$(vmstat 1 2 | tail -1 | awk '{printf $15}')
+cpu_op=$(expr 100 - $cpul)
+cpu_fin=$(printf "%.1f" $cpu_op)
 
 # LAST BOOT
-lb=$(who -b | awk '{print $3, $4}')
+lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
 
 # LVM USE
-lvmu=$(lsblk | grep -q "lvm" && echo yes || echo no)
+lvmu=$(if [ $(lsblk | grep "lvm" | wc -l) -gt 0 ]; then echo yes; else echo no; fi)
 
-# TCP CONNECTIONS
-tcpc=$(ss -ta state established | wc -l)
+# TCP CONNEXIONS
+tcpc=$(ss -ta | grep ESTAB | wc -l)
 
-# USER LOGGED
+# USER LOG
 ulog=$(users | wc -w)
 
 # NETWORK
-ip=$(hostname -I | awk '{print $1}')
-mac=$(ip link | awk '/link\/ether/ {print $2}')
+ip=$(hostname -I)
+mac=$(ip link | grep "link/ether" | awk '{print $2}')
 
-# SUDO COMMANDS
-cmnd=$(journalctl _COMM=sudo 2>/dev/null | grep COMMAND | wc -l)
+# SUDO
+cmnd=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
 
-# OUTPUT
-wall "
-Architecture: $arch
-CPU physical: $cpuf
-vCPU: $cpuv
-Memory Usage: ${ram_use}/${ram_total}MB (${ram_percent}%)
-Disk Usage: ${disk_use}/${disk_total} (${disk_percent}%)
-CPU load: ${cpu_load}%
-Last boot: $lb
-LVM use: $lvmu
-Connections TCP: $tcpc ESTABLISHED
-User log: $ulog
-Network: IP $ip ($mac)
-Sudo: $cmnd cmd
-"
+wall "Architecture: $arch
+	CPU physical: $cpuf
+	vCPU: $cpuv
+	Memory Usage: $ram_use/${ram_total}MB ($ram_percent%)
+	Disk Usage: $disk_use/${disk_total} ($disk_percent%)
+	CPU load: $cpu_fin%
+	Last boot: $lb
+	LVM use: $lvmu
+	Connections TCP: $tcpc ESTABLISHED
+	User log: $ulog
+	Network: IP $ip ($mac)
+	Sudo: $cmnd cmd"
